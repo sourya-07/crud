0eb26b996cd6e1b3739ea192d5fa0c6b2f55b05c
Sourya <souryagupta2005@gmail.com>
2025-12-16 15:40:19 +0530

    Created Basic express server and initialize nodemon

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const express = require("express")
+
+const app = express();
+
+const PORT = process.env.PORT || 5433;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, (() => {
+    console.log(`Server is running at PORT ${PORT}`)
+}))
